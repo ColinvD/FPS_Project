f1cb5cb92efa41f5c6c2ff0ddc69354baae5db05
KevinVanWezel <21973@ma-web.nl>
2017-11-16 08:48:09 +0100

    my changes

--- Assets/ColinStuff/Scripts/ColinScripts/UI/Kills/KillsAmount.cs
+++ Assets/ColinStuff/Scripts/ColinScripts/UI/Kills/KillsAmount.cs
@@ -4,29 +4,42 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class KillsAmount : MonoBehaviour {
-
+    [SerializeField]
+    private Door_Open Open;
     [SerializeField]
     private Text killsText;
+    private Text CoinsText;
     private int kills = 0;
+    private int Coins = 0;
 
     // Use this for initialization
     void Start () {
         killsText.text = kills.ToString();
-	}
+       // CoinsText.text = Coins.ToString();
+    }
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
-
-    public int getKills()
-    {
-        return kills;
-    }
+	
 
     public void AddKills ()
     {
+        Coins++;
         kills++;
         killsText.text = kills.ToString();
+      //  CoinsText.text = Coins.ToString();
     }
+    public void OpenDoor ()
+    {
+       
+            if (Coins >= 10)
+            {
+         
+            for (int i = 1; i <= 10; i++)
+            {
+                Coins--;
+            }
+            Open.Destroy();
+            }
+        }
+    
 }
---
+++ Assets/Scripts/overig/Door_Open.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door_Open : MonoBehaviour{
+    bool Dead = false;
+    [SerializeField] private KillsAmount currentKills;
+    
+    private void OnCollisionEnter(Collision other)
+    {
+
+        print(currentKills);
+        if (other.collider.tag == "wall")
+        {
+            currentKills.OpenDoor();
+            if (Dead == true)
+            {
+                Destroy(other.gameObject);
+                Dead = false;
+            }
+          
+            
+        }
+    }
+    public void Destroy()
+    {
+        Dead = true;
+    }
+
+
+}
