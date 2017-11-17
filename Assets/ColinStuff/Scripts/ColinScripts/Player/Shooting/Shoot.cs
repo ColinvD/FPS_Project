d7a7a77748c55bd4fad31ba8c97f34837c0636b3
Daan Ruting <daan.j.ruting@gmail.com>
2017-11-17 09:54:50 +0100

    inserting to database

--- Assets/ColinStuff/Scripts/ColinScripts/Player/Shooting/Shoot.cs
+++ Assets/ColinStuff/Scripts/ColinScripts/Player/Shooting/Shoot.cs
@@ -3,12 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Shoot : MonoBehaviour {
-    
+
+    private InsertShot insert;
     private InputManager inputManager;
+    private string object_hit;
+    private Vector3 begPos;
+    private Vector3 rot;
+    private Vector3 endPos;
     private int weapon = 1;
 
     void Start()
     {
+        if (!(insert = GetComponent<InsertShot>()))
+        {
+            insert = gameObject.AddComponent<InsertShot>();
+        }
         if (!(inputManager = GetComponent<InputManager>()))
         {
             inputManager = gameObject.AddComponent<InputManager>();
@@ -30,26 +39,31 @@ public class Shoot : MonoBehaviour {
             if (Input.GetMouseButtonDown(0))
             {
                 Shooting();
+                StartCoroutine(insert.DoPHP(object_hit, begPos, endPos, rot, 12345));
             }
         } else if(weapon == 2)
         {
             if (Input.GetMouseButton(0))
             {
                 Shooting();
+                StartCoroutine(insert.DoPHP(object_hit, begPos, endPos, rot, 12345));
             }
         }
     }
 
     public void Shooting()
     {
+        begPos = transform.position;
         Camera maincamera = FindObjectOfType<Camera>();
         Ray ray = new Ray(transform.position, maincamera.transform.forward);
+        rot = ray.direction;
         RaycastHit hit;
         Debug.DrawRay(ray.origin, ray.direction * 50, Color.green);
-        
         if(Physics.Raycast(ray, out hit, 50f))
         {
-            if(hit.collider.gameObject.tag == "Enemy")
+            object_hit = hit.collider.gameObject.tag;
+            endPos = hit.point;
+            if (hit.collider.gameObject.tag == "Enemy")
             {
                 hit.collider.GetComponent<EnemyHealth>().LoseLife(1, "Player");
             }
